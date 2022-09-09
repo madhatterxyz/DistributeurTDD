b83208b879f1536de1a457d5760621bff5f0ff42
Nicolas <Nicolas@PC-NICOLAS>
2022-09-09 14:16:48 +0200

    implement red second test

--- DistributeurTDD.Tests/BeverageTests.cs
+++ DistributeurTDD.Tests/BeverageTests.cs
@@ -14,5 +14,15 @@ namespace DistributeurTDD.Tests
             //Assert
             Assert.Equal(1.56, actualResult);
         }
+        [Fact]
+        public void GetPrice_Allongé_ShouldReturn182()
+        {
+            //Arrange
+            BeverageDomain domain = new BeverageDomain();
+            //Act
+            double actualResult = domain.GetPrice("Allongé");
+            //Assert
+            Assert.Equal(1.82, actualResult);
+        }
     }
 }

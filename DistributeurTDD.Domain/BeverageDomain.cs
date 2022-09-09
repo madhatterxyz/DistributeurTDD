cd125bff6d72c7433f633c9efad7efa4b0334513
Nicolas <Nicolas@PC-NICOLAS>
2022-09-09 14:14:15 +0200

    refactor first test - implement IngredientDomain

--- DistributeurTDD.Domain/BeverageDomain.cs
+++ DistributeurTDD.Domain/BeverageDomain.cs
@@ -4,8 +4,11 @@ namespace DistributeurTDD.Domain
 {
     public class BeverageDomain
     {
-        private IngredientDTO cafe = new IngredientDTO() { Id = 1, Name = "Café", UnitPrice = 1.00 };
-        private IngredientDTO eau = new IngredientDTO() { Id = 2, Name = "Eau", UnitPrice = 0.2 };
+        private readonly IngredientDomain _ingredientDomain;
+        public BeverageDomain()
+        {
+            _ingredientDomain = new IngredientDomain();
+        }
         private const double margin = 1.3;
         public double GetPrice(string v)
         {
@@ -13,6 +16,8 @@ namespace DistributeurTDD.Domain
             switch(v)
             {
                 case "Expresso":
+                    IngredientDTO cafe = _ingredientDomain.Get("Café");
+                    IngredientDTO eau = _ingredientDomain.Get("Eau");
                     price = (cafe.UnitPrice + eau.UnitPrice)*margin;
                     break;
                 default:break;
---
+++ DistributeurTDD.Domain/IngredientDomain.cs
@@ -0,0 +1,25 @@
+﻿using DistributeurTDD.Domain.DTO;
+
+namespace DistributeurTDD.Domain
+{
+    public class IngredientDomain
+    {
+        private List<IngredientDTO> _ingredientDTOs { get; set; }
+        public IngredientDomain()
+        {
+            _ingredientDTOs = new List<IngredientDTO>()
+            {
+                new IngredientDTO() { Id = 1, Name = "Café", UnitPrice = 1.00 },
+                new IngredientDTO() { Id = 2, Name = "Eau", UnitPrice = 0.2 }
+            };
+        }
+        public List<IngredientDTO> GetAll()
+        {
+            return _ingredientDTOs;
+        }
+        public IngredientDTO Get(string name)
+        {
+            return _ingredientDTOs.FirstOrDefault(x => x.Name == name);
+        }
+    }
+}

53cee9f95a33012f910a083ec6713bd4b7146497
Nicolas <Nicolas@PC-NICOLAS>
2022-09-09 13:49:14 +0200

    implement green test

--- DistributeurTDD.Domain/BeverageDomain.cs
+++ DistributeurTDD.Domain/BeverageDomain.cs
@@ -2,9 +2,20 @@
 {
     public class BeverageDomain
     {
+        private const double coffeeUnitPrice = 1.00;
+        private const double waterUnitPrice = 0.2;
+        private const double margin = 1.3;
         public double GetPrice(string v)
         {
-            throw new NotImplementedException();
+            double price = 0;
+            switch(v)
+            {
+                case "Expresso":
+                    price = (coffeeUnitPrice + waterUnitPrice)*margin;
+                    break;
+                default:break;
+            }
+            return price;
         }
     }
 }
